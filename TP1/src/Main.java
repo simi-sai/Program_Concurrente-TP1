12c7358d9505b87d73d2605d744603cf7972625d
rodrigov1 <rodrigo.vargas@mi.unc.edu.ar>
2024-04-09 17:02:13 -0300

    feat: matriz de asientos implementada

--- TP1/src/Main.java
+++ TP1/src/Main.java
@@ -2,10 +2,11 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
   public static void main(String[] args) {
-    System.out.println("Hola Mundo");
-    int a = 5;
-    System.out.println(a);
-    int b = 7;
-    System.out.println(b + a);
+    // Creamos la matriz de asientos 31x6 = 186 asientos y lo inicializamos en 0
+    // 0: Libre
+    // 1: Ocupado
+    // -1: Descartado
+    int[][] array = new int[31][6];
+    System.out.println("Probando si anda");
   }
 }

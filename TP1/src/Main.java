bbefb6297e8265ce5d3a9dd913817f3e0151c198
rodrigov1 <rodrigo.vargas@mi.unc.edu.ar>
2024-04-14 19:19:04 -0300

    feat: Matriz de asientos implementada en EtapaReserva, main ahora solo inicializa la etapa

--- TP1/src/Main.java
+++ TP1/src/Main.java
@@ -1,12 +1,9 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import tareas.EtapaReserva;
+// import tareas.Registros;
+
 public class Main {
   public static void main(String[] args) {
-    // Creamos la matriz de asientos 31x6 = 186 asientos y lo inicializamos en 0
-    // 0: Libre
-    // 1: Ocupado
-    // -1: Descartado
-    int[][] array = new int[31][6];
-    System.out.println("Probando si anda");
+    EtapaReserva etapa = new EtapaReserva();
+    etapa.ejecutarEtapa();
   }
 }

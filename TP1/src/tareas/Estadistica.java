a9e33fefd32a79329ef4c1467be39b9a1a3221e5
rodrigov1 <rodrigo.vargas@mi.unc.edu.ar>
2024-04-20 14:53:56 -0300

    fix: Estadistica now writes at its last iteration

--- TP1/src/tareas/Estadistica.java
+++ TP1/src/tareas/Estadistica.java
@@ -15,43 +15,60 @@ public class Estadistica implements Runnable {
 
   @Override
   public void run() {
-    int totalVerif, totalCanceladas, totalReservas, randomNum;
-    Random random = new Random();
+    int totalVerif, totalCanceladas, totalReservas;
     while (true) {
-      totalVerif = registros.getVerificadas_size();
-      totalCanceladas = registros.getCanceladas_size();
-      totalReservas = totalVerif + totalCanceladas;
-      randomNum = random.nextInt(500);
-      if (totalReservas == 186) {
+      if (allSeatsVerified()) {
         try {
-          Thread.sleep(randomNum); // Wait up to 1 second
+          FileWriter fw = new FileWriter("statistics.txt", true); // Append to the file
+          PrintWriter writer = new PrintWriter(fw);
+          totalVerif = registros.getVerificadas_size();
+          totalCanceladas = registros.getCanceladas_size();
+          totalReservas = totalVerif + totalCanceladas;
+          // TODO: Imprimir ocupacion final del vuelo
+          // TODO: Analisis estadistico dps de multiples ejecuciones con conclusiones
+          writer.printf("Reservations Verified: %d, Cancelled: %d, Total: %d\n",
+              totalVerif, totalCanceladas,
+              totalReservas);
+          writer.println("------------------"); // Write a line to the file
+          writer.flush();
+          writer.close(); // Close the writer
+        } catch (IOException e) {
+          e.printStackTrace();
+        }
+        break;
+      } else {
+        try {
+          FileWriter fw = new FileWriter("statistics.txt", true); // Append to the file
+          PrintWriter writer = new PrintWriter(fw);
+          totalVerif = registros.getVerificadas_size();
+          totalCanceladas = registros.getCanceladas_size();
+          totalReservas = totalVerif + totalCanceladas;
+          writer.printf("Reservations Verified: %d, Cancelled: %d, Total: %d\n",
+              totalVerif, totalCanceladas,
+              totalReservas);
+          writer.println("------------------"); // Write a line to the file
+          writer.flush();
+          writer.close(); // Close the writer
+        } catch (IOException e) {
+          e.printStackTrace();
+        }
+        try {
+          Thread.sleep(200);
         } catch (InterruptedException e) {
           e.printStackTrace();
         }
-        totalVerif = registros.getVerificadas_size();
-        totalCanceladas = registros.getCanceladas_size();
-        totalReservas = totalVerif + totalCanceladas;
-        if (totalReservas == 186) {
-          break;
-        }
-      }
-      try {
-        FileWriter fw = new FileWriter("statistics.txt", true); // Append to the file
-        PrintWriter writer = new PrintWriter(fw);
-        writer.printf("Reservations Verified: %d, Cancelled: %d, Total: %d\n",
-            totalVerif, totalCanceladas,
-            totalReservas);
-        writer.println("------------------"); // Write a line to the file
-        writer.close(); // Close the writer
-      } catch (IOException e) {
-        e.printStackTrace();
-      }
-      try {
-        Thread.sleep(200);
-      } catch (InterruptedException e) {
-        e.printStackTrace();
       }
     }
   }
 
+  private boolean allSeatsVerified() {
+    int totalVerif = registros.getVerificadas_size();
+    int totalCanceladas = registros.getCanceladas_size();
+    int totalReservas = totalVerif + totalCanceladas;
+    if (totalReservas == 186) {
+      return true;
+    } else {
+      return false;
+    }
+  }
 }

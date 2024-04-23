7a7f7ad642f7efe29c1197bb26c9b04683a5f1d5
rodrigov1 <rodrigo.vargas@mi.unc.edu.ar>
2024-04-22 23:27:49 -0300

    fix: minor fix in Estadistica

--- TP1/src/tareas/Estadistica.java
+++ TP1/src/tareas/Estadistica.java
@@ -3,7 +3,6 @@ package tareas;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.PrintWriter;
-import java.util.Random;
 
 public class Estadistica implements Runnable {
   private Registros registros;

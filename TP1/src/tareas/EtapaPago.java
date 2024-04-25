0210b0af585bc77e4719b1dd29693e5541d1672f
rodrigov1 <rodrigo.vargas@mi.unc.edu.ar>
2024-04-25 18:05:30 -0300

    feat: wait-notifyAll added in Registros methods (get_reserva / registrar_reserva) and some changes on time values for testing purposes

--- TP1/src/tareas/EtapaPago.java
+++ TP1/src/tareas/EtapaPago.java
@@ -4,7 +4,7 @@ import java.util.Random;
 
 public class EtapaPago {
   private Registros registros;
-  private static final int DURACION_ITERACION = 300; // 40 milisegundos
+  private static final int DURACION_ITERACION = 30; // 40 milisegundos
 
   public EtapaPago(Registros registros) {
     this.registros = registros;
@@ -20,7 +20,7 @@ public class EtapaPago {
       while (true) {
         if (noMoreSeats()) {
           try {
-            Thread.sleep(random.nextInt(1000, 3000)); // 2-4 sg
+            Thread.sleep(random.nextInt(100, 300)); // 2-4 sg
           } catch (InterruptedException e) {
             e.printStackTrace();
           }
@@ -32,10 +32,9 @@ public class EtapaPago {
         // Takes a random reservation from the pending list
         Asiento randomAsiento = registros.get_reserva(0);
         int randomNumber = random.nextInt(100);
-        if (randomAsiento != null) {
-          // Synchronize on the randomAsiento to avoid conflicts with other threads
-          synchronized (randomAsiento) {
-            registros.eliminar_reserva(0, randomAsiento); // Se elimina de la lista de pendientes
+        // Synchronize on the randomAsiento to avoid conflicts with other threads
+        synchronized (randomAsiento) {
+          if (registros.eliminar_reserva(0, randomAsiento)) { // Se elimina de la lista de pendientes
             if (randomNumber < 90) {
               // Aprobado
               randomAsiento.setEstado(1);
@@ -45,8 +44,6 @@ public class EtapaPago {
               registros.registrar_reserva(1, randomAsiento); // Se agrega a la lista de reservas canceladas
             }
           }
-        } else {
-          continue;
         }
         try {
           Thread.sleep(DURACION_ITERACION);
